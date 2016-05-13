5dccf626a43844f52d90877804379b3609bb5529
Glen Hayes <glen@redcitycreative.com>
2016-05-13 16:25:45 -0600

    adding user auth

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'glen@redcitycreative.com', 'name' => '100 Men Team'],
 
     /*
     |--------------------------------------------------------------------------
