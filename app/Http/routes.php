ac2057a7792c78e7d1de3ce0d4551732ab56f49b
Glen Hayes <glen@redcitycreative.com>
2016-05-20 11:51:15 -0600

    clean up routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -12,83 +11,33 @@
 |
 */
 
-
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
-
     Route::get('/', function () {
         return view('welcome');
     });
-    Route::get('/new-member', function () {
-        return view('pages.new-member');
-    });
-    Route::get('/returning-member', function () {
-        return view('pages.returning-member');
-    });
-
-    Route::get('/payment', function () {
-        return view('pages.payment');
-    });
-    Route::get('/thank-you-checkin', function () {
-        return view('pages.thank-you-checkin');
-    });
-    Route::get('/secret-code', function () {
-        return view('pages.secret-code');
-    });
-    Route::get('/vote', function () {
-        return view('pages.vote');
-    });
-    Route::get('/thank-you-vote', function () {
-        return view('pages.thank-you-vote');
-    });
-
-    Route::get('/live-vote', function () {
-        return view('pages.live-vote');
-    });
-    Route::get('/100-admin', function () {
-        return view('pages.100-admin');
-    });
-
     Route::get('/home', 'HomeController@index');
-
     Route::get('/events', 'EventsController@index');
-
-    // Route::get('/create-event', function () {
-    //     return view('pages.create-event');
-    // });
-
+    
     // Admin Pages
 
-    Route::get('/create-event', ['middleware' => 'auth', function() {
-    }]);
-
-    Route::get('/report', ['middleware' => 'auth', function() {
-    }]);
-
     Route::get('/dashboard', 'DashboardController@index');
-
     Route::get('events/{event}', [
         'middleware' => 'auth',
         'uses' => 'EventsController@show'
     ]);
-
     Route::get('events/add', [
         'middleware' => 'auth',
         'uses' => 'EventsController@addEvent'
     ]);
-    
     Route::post('events/{event}/charities',[
         'middleware' => 'auth',
         'uses' => 'CharitiesController@store'
      ]);
-
     Route::get('charities/{charity}/edit', [
         'middleware' => 'auth',
         'uses' => 'CharitiesController@edit'
     ]);
+    Route::get('charities/{charity}/delete', 'CharitiesController@delete');
 
- Route::get('charities/{charity}/delete', 'CharitiesController@delete');
-
-    
 });
