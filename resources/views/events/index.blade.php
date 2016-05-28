5bca3b15bc710933d7cef316d92e0e5b2960257c
Glen Hayes <glen@redcitycreative.com>
2016-05-28 15:43:05 -0600

    bootstraping index event page

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -6,38 +6,38 @@
 	
 
 	@foreach ($events as $event)
-	<div class="event">
-	@if($event->id == $currentEvent->id )
-			
-			<a href="#" class='btn btn-danger pull-right'>Current Event</a>
-			<a href="/events/{{ $event->id }}/passcode" class='btn btn-danger pull-right'>show passcode</a>
-		@else
-			<a href="/event/{{ $event->id }}/current" class='btn btn-primary pull-right'>Make Current Event</a>
-		@endif
-		<h3><a href="/events/{{ $event->id }}" class="current-event">{{ $event->title }}</a></h3>
-		<p><strong>Event details</strong></p>
-		<p>{{ $event->location }} <br> {{$event->start_time}}</p>
-		<p><strong>Charities</strong></p>
-		@if(count($event->charities) > 0)
-			
-			@foreach($event->charities as $charity)
-					<p>{{ $charity->title}}</p>
-								
-
-			@endforeach
-			@if(count($event->charities) < 3)
-				<p><a href="/events/{{ $event->id }}" class="current-event">Add More Charities</a></p>
+	<div class="event row">
+		<div class="col-xs-10">
+			<h3><a href="/events/{{ $event->id }}" class="current-event">{{ $event->title }}</a></h3>
+			<h4 class="text-muted">Event details</h4>
+			<address>
+				<strong>{{ $event->location }}</strong><br> 
+				{{$event->start_time}}
+			</address>
+			<h4>Charities</h4>
+			@if(count($event->charities) > 0)
+				@foreach($event->charities as $charity)
+						<p>{{ $charity->title}}</p>
+				@endforeach
+				@if(count($event->charities) < 3)
+					<p><a href="/events/{{ $event->id }}" class="current-event">Add More Charities</a></p>
+				@endif
+			@else
+				<p class="text-muted">No Charities have been added to event</p>
+				<p><a href="/events/{{ $event->id }}" class="current-event">Add Charities</a></p>	
 			@endif
-		@else
-		<p><mark>No Charities have been added to event</mark></p>
-		<p><a href="/events/{{ $event->id }}" class="current-event">Add Charities</a></p>	
-		@endif
-		
+		</div>
+		<div class="col-xs-2">
+			<p class="text-right">
+				@if($event->id == $currentEvent->id )
+					<a href="#" class='btn btn-danger pull-right'>Current Event</a><br>
+					<a href="/events/{{ $event->id }}/passcode" class='btn btn-danger'>Show Passcode</a>	
+				@else
+					<a href="/event/{{ $event->id }}/current" class='btn btn-primary pull-right clear'>Make Current Event</a>
+				@endif
+			</p>
+		</div>		
 	</div>
+	<hr>
 	@endforeach
-
-	
-
-	
-
 @endsection
