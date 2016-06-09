54b05835d48202be06999f323d7d9e16df0e817c
Glen Hayes <glen@redcitycreative.com>
2016-06-08 21:33:09 -0600

    took out extra redcity in footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -6,10 +6,7 @@
         <a href="http://facebook.com"><i class="fa fa-twitter" aria-hidden="true"></i></i></a> 
         <a href="http://facebook.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
     </div>
-
-    <p id=siteby >site by: <a href="www.redcitycreative.com">RedCity Creative</a></p>
-    <p>100 Men YYC &copy; {{ date("Y") }} </p>
-
-     <p id=siteby>Website By: <a href="www.redcitycreative.com"><img src="/images/RCC_Footer.jpeg" style="width:100px;" /></a></p>
+	<p>100 Men YYC &copy; {{ date("Y") }} </p>
+	<p id=siteby>Website By: <a href="www.redcitycreative.com"><img src="/images/RCC_Footer.jpeg" style="width:100px;" /></a></p>
 
 </footer>
