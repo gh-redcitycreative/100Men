adc21a5859fee3332adf7016abcf2cf6fed90d79
Krista Gillies <krista@redcitycreative.com>
2016-06-13 11:59:32 -0600

    registratiin email, donate updates

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -3,7 +3,7 @@
 @section('header')
 <header>
 	<div class="container">
-		<h1>Charity Donations for 100Men Calgary</h1>
+		<h1>Donate Now</h1>
 	</div>
 </header>
 
@@ -19,7 +19,7 @@
         </div>
 		<div class="row ">
 			<div class="col-xs-12">
-				<h2>100 Men Charity Donations</h2>
+				<h2>Online Donations</h2>
 				<p>Note: 100 Men Calgary now accepts your donations directly, holds them for the charity, and provides one lump sum donation to the charity that includes a list of people that make up that donation.</p>
 				<p>As such, we have added $3.30 for credit card processing costs on top of the $100 donation. This ensures that your $100 makes it to the receiving charity and that you get a tax receipt for it.</p>
 			</div>
@@ -35,6 +35,7 @@
 				    data-name="100 Men Who Give A Damn Society Of Alberta"
 				    data-description="Charity Donation"
 				    data-image="https://s3.amazonaws.com/stripe-uploads/acct_17lPfaBU2zDDaeommerchant-icon-1457124321408-600x600.png"
+				    data-label="Donate with Card"
 				    data-locale="auto"
 				    data-currency="cad">
 				  </script>
