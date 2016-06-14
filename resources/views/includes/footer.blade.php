3ea90c99531d825afc7f8ef1a6eddf306fb92893
Glen Hayes <glen@redcitycreative.com>
2016-06-13 20:46:00 -0600

    footer small changes padding and clean up code

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,15 +1,10 @@
-<footer>
-    
-    <div class="socials">
-    	<h3 class="lead">Follow us on Social Media</h3>
-        <a href="https://www.facebook.com/100mencalgary"><i class="fa fa-facebook" aria-hidden="true"></i></i></a> 
-        <a href="https://twitter.com/100MenCalgary"><i class="fa fa-twitter" aria-hidden="true"></i></i></a> 
-        <a href="https://www.instagram.com/100mencalgary/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+<footer>   
+	<div class="socials">
+    	<h3 class="lead">Follow Us on Social Media</h3>
+		<a href="https://www.facebook.com/100mencalgary"><i class="fa fa-facebook" aria-hidden="true"></i></a> 
+		<a href="https://twitter.com/100MenCalgary"><i class="fa fa-twitter" aria-hidden="true"></i></a> 
+		<a href="https://www.instagram.com/100mencalgary/"><i class="fa fa-instagram" aria-hidden="true"></i></a> 
     </div>
 	<p>100 Men Calgary &copy; {{ date("Y") }} </p>
-	<p id=siteby>Webapp By: <a href="www.redcitycreative.com"><img src="/images/RCC_Footer.jpeg" style="width:100px;" /></a></p>
-
-
-	
-
+	<p id=siteby>Web App By: <a href="www.redcitycreative.com"><img src="/images/RCC_Footer.jpeg" style="width:100px;" /></a></p>
 </footer>
