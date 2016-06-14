08d3352c1f38af9089461eba28dd0e87b72833a2
Glen Hayes <glen@redcitycreative.com>
2016-06-14 17:42:20 -0600

    stripe passcode change

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -17,20 +17,14 @@
                 <img src="/images/100-Men-Logo-White.png" alt="#">
             </div>
         </div>
-		<div class="row ">
-			<div class="col-xs-12">
-				<h2>Online Donations</h2>
-				<p>Note: 100 Men Calgary now accepts your donations directly, holds them for the charity, and provides one lump sum donation to the charity that includes a list of people that make up that donation.</p>
-				<p>As such, we have added $3.30 for credit card processing costs on top of the $100 donation. This ensures that your $100 makes it to the receiving charity and that you get a tax receipt for it.</p>
-			</div>
-		</div>
+	
 		<div class="row">
 			<div class="col-xs-12">
 				<form action="/stripe/payment" method="POST">
 				 {{ csrf_field() }}
 				  <script
 				    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-				    data-key="pk_test_e9iswNE3T0C1LoCFFFt29pEl"
+				    data-key="pk_live_g5a87a8s4QtIcze6ZBpugphR"
 				    data-amount="10300" 
 				    data-name="100 Men Who Give A Damn Society Of Alberta"
 				    data-description="Charity Donation"
@@ -42,5 +36,12 @@
 				</form>
 			</div>
 		</div>	
+			<div class="row ">
+			<div class="col-xs-12">
+				<h2>Online Donations</h2>
+				<p>Note: 100 Men Calgary now accepts your donations directly, holds them for the charity, and provides one lump sum donation to the charity that includes a list of people that make up that donation.</p>
+				<p>As such, we have added $3.30 for credit card processing costs on top of the $100 donation. This ensures that your $100 makes it to the receiving charity and that you get a tax receipt for it.</p>
+			</div>
+		</div>
 	</div>
 @endsection
